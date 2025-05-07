be459bc5e43683be4889cc23ff1bb76010b7718b
omidi <omidi@nsec.ir>
2025-05-07 17:52:32 +0330

    Working on loading files

--- app/(app)/page.jsx
+++ app/(app)/page.jsx
@@ -1,5 +1,5 @@
 export default function App() {
-  throw new Error("This is a test error");
+  //   throw new Error("This is a test error");
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
