0755e1efd5c32497793364f7cf8297505c269cc2
Alireza Omidi <omidi@nsec.ir>
2025-05-18 22:51:51 +0330

    Updated the tailwind version to 4.1.7

--- app/components/SearchForm.jsx
+++ app/components/SearchForm.jsx
@@ -1,7 +1,12 @@
 export default function SearchForm() {
   return (
     <form onSubmit="/search" className="flex gap-2">
-      <input type="text" name="search" className="border-2 border-gray-800 rounded-md p-2 text-gray-800"/>
+      <input 
+      type="text" 
+      name="search" 
+      className="border-2 border-gray-800 rounded-md p-2 text-gray-800 bg-gray-100"
+      placeholder="Search"
+      />
       <button type="submit" className="submit-button-class border-2 border-gray-800">Search</button>
     </form>
   );
