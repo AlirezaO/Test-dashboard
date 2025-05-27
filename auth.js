d620140cafc02b23b0d08aa0fbb289288fd957c9
omidi <omidi@nsec.ir>
2025-05-27 17:26:19 +0330

    Fixed the warnings on start

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  secret: process.env.AUTH_SECRET,
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
+  // swcMinify: true,
+  // experimental: {
+  //   appDir: true,
+  // },
   images: {
     //This will let nextJs to trust and use external images in the project
     dangerouslyAllowSVG: true,
